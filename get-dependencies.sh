040582c4a0467ff27cfe493d39d3cc06f4721f8a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-19 09:48:09 -0400

    build the thing

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \ 
+	gst-plugins-bad  \
+	gst-plugins-base \
+	gst-plugins-good \
+	gst-plugins-ugly \
+	gst-plugin-va    \
+	pipewire-audio   \
+	pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package webcamoid
 
 # If the application needs to be manually built that has to be done down here
