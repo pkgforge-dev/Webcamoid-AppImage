0df0701307c8f15e6d229993bc6fef9332037874
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-19 10:00:54 -0400

    app depends on another AUR package

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -20,6 +20,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
+make-aur-package libuvc
 make-aur-package webcamoid
 
 # If the application needs to be manually built that has to be done down here
